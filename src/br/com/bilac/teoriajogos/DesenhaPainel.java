ffb81aadb39dee1e9d1a67849419ac611040dd08
Rômulo Lima <romulolink@gmail.com>
2015-02-05 21:50:49 -0200

    versao4 circulos

--- src/br/com/bilac/teoriajogos/DesenhaPainel.java
+++ src/br/com/bilac/teoriajogos/DesenhaPainel.java
@@ -9,9 +9,9 @@ public class DesenhaPainel extends JPanel {
     public void paintComponent(Graphics g){
 
         super.paintComponent(g);
-        for (int i = 0; i < 15; i++){
-            g.drawLine(10+i*10, 10+i*10, 50+i*10,50+i*10);
-            
-        }
+        g.setColor(Color.yellow);
+        g.fillOval(10,10,100,100);
+        g.setColor(Color.red);
+        g.drawOval(10,10,100,100);
     }
 }
