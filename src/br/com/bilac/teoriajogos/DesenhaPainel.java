d032c458fa20587d67029f5aa55cc493dcf5521d
Rômulo Lima <romulolink@gmail.com>
2015-02-05 21:47:41 -0200

    versao3

--- src/br/com/bilac/teoriajogos/DesenhaPainel.java
+++ src/br/com/bilac/teoriajogos/DesenhaPainel.java
@@ -9,9 +9,9 @@ public class DesenhaPainel extends JPanel {
     public void paintComponent(Graphics g){
 
         super.paintComponent(g);
-        int largura = getWidth();
-        int altura = getHeight();
-        g.drawLine(0,0,largura,altura);
-        g.drawLine(0,altura,largura,0);
+        for (int i = 0; i < 15; i++){
+            g.drawLine(10+i*10, 10+i*10, 50+i*10,50+i*10);
+            
+        }
     }
 }
